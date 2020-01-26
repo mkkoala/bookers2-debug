0fc70010393c2ffd6d3235d23f1646ddced324e6
mkkoala <mako1105.96@gmail.com>
2020-01-26 14:46:37 +0000

    コメント機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+
+  resources :books do
+  	resource :book_comments, only: [:create, :destroy]
+  	resources :favorites, only: [:create, :destroy]
+  end
 
   root 'home#top'
   get 'home/about'
