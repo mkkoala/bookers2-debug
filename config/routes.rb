70db764a4e75f4c4bd0b06a3142efb14647fb780
mkkoala <mako1105.96@gmail.com>
2020-01-27 12:26:10 +0000

    フォロー、フォロワー機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      get :following,:followers
+    end
+  end
 
   resources :books do
   	resource :book_comments, only: [:create, :destroy]
   	resources :favorites, only: [:create, :destroy]
   end
+
+  resources :relationships, only: [:create, :destroy]
 
   root 'home#top'
   get 'home/about'
